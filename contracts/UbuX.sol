36179fd9810fe6e68c20d2f4b82bd4ae01c3f1f0
Jameel Mukadam <jameel@jameelmukadam.com>
2017-09-21 10:04:03 +0200

    Feature: added ownership to contract\
    Feature: added basic tests for UbuX token

--- contracts/UbuX.sol
+++ contracts/UbuX.sol
@@ -3,14 +3,26 @@ import './zeppelin-solidity/contracts/token/StandardToken.sol';
 
 contract UbuX is StandardToken {
 
-  string public name = 'UBUx Token';
-  string public symbol = 'UBx';
-  uint public decimals = 4;
-  uint public INITIAL_SUPPLY = 230000000;
+  string public constant name = 'UBUx Token';
+  string public constant symbol = 'UBx';
+  uint8 public constant decimals = 4;
+  uint256 public INITIAL_SUPPLY = 230000000;
+
+  // Owner of this contract
+  address public owner;
+
+  // Functions with this modifier can only be executed by the owner
+  modifier onlyOwner() {
+    if (msg.sender != owner) {
+      throw;
+    }
+    _;
+  }
 
   function UbuX() {
     totalSupply = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
+    owner = msg.sender;
+    balances[owner] = INITIAL_SUPPLY;
   }
 
 }
