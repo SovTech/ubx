18742683450c03249bf0ab917004ff761aaf6823
Jameel Mukadam <jameel@jameelmukadam.com>
2017-09-22 10:59:55 +0200

    Feature: added a local example folder for full code of the contract including open zeppelin contracts
    Feature: added live test server details to truffle

---
+++ full_contract_example/UbuX.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.4;
+import './StandardToken.sol';
+
+contract UbuX is StandardToken {
+
+  string public constant name = 'UBUx Token';
+  string public constant symbol = 'UBx';
+  uint8 public constant decimals = 4;
+  uint256 public INITIAL_SUPPLY = 230000000;
+
+  function UbuX() {
+    totalSupply = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
+
+}
--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    live: {
+      host: "188.166.98.198",
+      port: 8545,
+      network_id: "1" // Match any network id
     }
   }
 };
